94052ef9deb45565d47947fc9d776e7028c96f42
Maple <maple.zhu7@gmail.com>
2022-06-10 13:34:27 -0700

    Add data validation and more variable for user DB

--- models/user.js
+++ models/user.js
@@ -1,15 +1,38 @@
 import mongoose from 'mongoose';
+import validator from "validator";
 
 const userSchema = new mongoose.Schema({
+  firstName:{
+    type: String,
+    required: [true, 'Please provide name'],
+    minlength: 3,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    default: 'lastName',
+  },
+  location: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: 'Northeastern',
+  },
   email: {
     type: String,
-    required: true,
+    required: [true, 'Please provide email'],
+    validate: {
+      validator: validator.isEmail,
+      message: 'Please provide a valid email',
+    },
     trim: true,
     unique: true,
   },
   password: {
     type: String,
-    required: true,
+    required: [true, 'Please provide password'],
+    minlength: 6,
     trim: true,
   },
 });
