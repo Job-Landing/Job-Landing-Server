7237644ae2259b2c25e358ebaf18c5832c4a0afa
wayson <wayson1225@outlook.com>
2022-06-09 19:55:49 -0700

    add RUD operations to User Model

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -11,4 +11,33 @@ const createUser = async (req, res) => {
   }
 };
 
-export default createUser;
+const getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(StatusCodes.OK).send(user);
+  } catch (error) {
+    res.status(StatusCodes.BAD_REQUEST).send(error);
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(StatusCodes.OK).send(user);
+  } catch (error) {
+    res.status(StatusCodes.BAD_REQUEST).send(error);
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(StatusCodes.OK).send({ message: 'User deleted' });
+  } catch (error) {
+    res.status(StatusCodes.BAD_REQUEST).send(error);
+  }
+};
+
+export { createUser, getUser, updateUser, deleteUser };
