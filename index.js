6cf9597284952d178700827b2ef854fdac7569c0
Maple <maple.zhu7@gmail.com>
2022-06-10 13:23:14 -0700

    Create bd connect.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import app from './app.js';
 import 'dotenv/config';
-import mongoose from 'mongoose';
+import connectDB from './db/connect.js';
 
 const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await connectDB(process.env.MONGO_URL);
     console.log('Successfully connected to MongoDB.');
   } catch (error) {
     console.log(error);
