4a774f3505b81b1634f74bad32216a9f696040d4
wayson <wayson1225@outlook.com>
2022-06-09 18:42:47 -0700

    add create user option

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,1 +1,13 @@
 // CRUD operations for User
+import User from '../models/user';
+
+const createUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export default createUser;
--- routes/user/signup.js
+++
@@ -1,3 +0,0 @@
-import User from '../../models/user';
-
-const user = new User();
