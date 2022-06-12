d24a3eedd6f68785faf7db2e497d0c1cc0af8e46
wayson <wayson1225@outlook.com>
2022-06-12 15:18:46 -0700

    Add: add stream model

---
+++ models/stream.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const streamSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: [true, 'Please provide company'],
+    maxlength: 50,
+  },
+  position: {
+    type: String,
+    required: [true, 'Please provide position'],
+    maxlength: 100,
+  },
+  applyUrl: {
+    type: String,
+    required: [true, 'Please provide apply url'],
+    validate: {
+      validator: validator.isURL,
+      message: 'Please provide a valid url',
+    },
+    maxlength: 200,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
+  jobId: {
+    type: mongoose.Types.ObjectId,
+    required: [true, 'Please provide job id'],
+    maxlength: 100,
+  },
+});
+
+const Stream = mongoose.model('Stream', streamSchema);
+
+export default Stream;
