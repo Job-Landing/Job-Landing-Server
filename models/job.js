5099e71f1c286b4293c96c5b52cfcdd1f16c6039
wayson <wayson1225@outlook.com>
2022-06-12 14:51:36 -0700

    Add: add optional applyUrl field in Job Model

--- models/job.js
+++ models/job.js
@@ -1,38 +1,47 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+import validator from 'validator';
 
 const jobSchema = new mongoose.Schema({
-    company: {
-        type: String,
-        required: [true, 'Please provide company'],
-        maxlength: 50,
+  company: {
+    type: String,
+    required: [true, 'Please provide company'],
+    maxlength: 50,
+  },
+  position: {
+    type: String,
+    required: [true, 'Please provide position'],
+    maxlength: 100,
+  },
+  status: {
+    type: String,
+    enum: ['interview', 'declined', 'pending'],
+    default: 'pending',
+  },
+  jobType: {
+    type: String,
+    enum: ['full-time', 'part-time', 'remote', 'internship'],
+    default: 'full-time',
+  },
+  jobLocation: {
+    type: String,
+    default: 'Northeastern',
+    required: true,
+  },
+  createdBy: {
+    type: String,
+    required: true,
+    // type: mongoose.Types.ObjectId,
+    // ref: 'User',
+    // required: [true, 'Please provide user'],
+  },
+  applyUrl: {
+    type: String,
+    validate: {
+      validator: validator.isURL,
+      message: 'Please provide a valid url',
     },
-    position: {
-        type: String,
-        required: [true, 'Please provide position'],
-        maxlength: 100,
-    },
-    status: {
-        type: String,
-        enum: ['interview', 'declined', 'pending'],
-        default: 'pending',
-    },
-    jobType: {
-        type: String,
-        enum: ['full-time', 'part-time', 'remote', 'internship'],
-        default: 'full-time',
-    },
-    jobLocation: {
-        type: String,
-        default: 'Northeastern',
-        required: true,
-    },
-    createdBy: {
-        type: String,
-        required:true
-        // type: mongoose.Types.ObjectId,
-        // ref: 'User',
-        // required: [true, 'Please provide user'],
-    }
+    maxlength: 200,
+  },
 });
 
-export default mongoose.model('Job', jobSchema)
+export default mongoose.model('Job', jobSchema);
