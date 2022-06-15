c9868688a818e56885f9a11d30c84c983b9c8519
wayson <wayson1225@outlook.com>
2022-06-14 21:54:52 -0700

    Add: complete the signout router

--- routes/user/signout.js
+++ routes/user/signout.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 router.post('/user/signout', async (req, res) => {
   console.log('signout');
+  req.session = null;
+  res.send({});
 });
 
 export { router as signoutRouter };
