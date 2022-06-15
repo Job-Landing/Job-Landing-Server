5d83dd01f3eeef295d341822ab6f97cce4855341
wayson <wayson1225@outlook.com>
2022-06-15 00:12:27 -0700

    Update: simplify the user model definition

--- models/user.js
+++ models/user.js
@@ -5,31 +5,19 @@ import Password from '../utils/password.js';
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: [true, 'Please provide username'],
-    minlength: 4,
-    trim: true,
-    unique: true,
+    required: true,
   },
   email: {
     type: String,
-    required: [true, 'Please provide email'],
-    validate: {
-      validator: validator.isEmail,
-      message: 'Please provide a valid email',
-    },
-    trim: true,
-    unique: true,
+    required: true,
   },
   password: {
     type: String,
-    required: [true, 'Please provide password'],
-    minlength: 6,
-    trim: true,
+    required: true,
   },
   location: {
     type: String,
-    trim: true,
-    maxlength: 20,
+    required: true,
   },
 });
 
