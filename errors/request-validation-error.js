853069e432ca01f950b7d812aab41163f0fb5285
wayson <wayson1225@outlook.com>
2022-06-14 23:37:02 -0700

    Add: add RequestValidationError

---
+++ errors/request-validation-error.js
@@ -0,0 +1,18 @@
+import CustomError from './custom-error';
+import { StatusCodes } from 'http-status-codes';
+
+class RequestValidationError extends CustomError {
+  statusCode = StatusCodes.BAD_REQUEST;
+  constructor(message = 'Validation failed', errors) {
+    super(message);
+    this.errors = errors;
+    Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+  serializeErrors() {
+    return this.errors.map((error) => {
+      return { message: error.msg };
+    });
+  }
+}
+
+export default RequestValidationError;
