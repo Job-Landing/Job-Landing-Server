4925ad987aa62f10598b10f5b5c7c6a55b32967f
wayson <wayson1225@outlook.com>
2022-06-23 11:28:49 -0700

    Update: Redesigned the job model

--- models/job.js
+++ models/job.js
@@ -1,32 +1,46 @@
 import mongoose from 'mongoose';
-import validator from 'validator';
 
 const jobSchema = new mongoose.Schema({
-  company: {
-    type: String,
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
     required: true,
   },
-  position: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-  },
-  type: {
-    type: String,
-  },
-  location: {
-    type: String,
-  },
-  applyUrl: {
-    type: String,
-    // may change to not required
-    required: true,
-  },
-  createdBy: {
-    type: String,
-    required: true,
+  job_list: {
+    _id: {
+      type: mongoose.Schema.Types.ObjectId,
+      default: mongoose.Types.ObjectId,
+      required: true,
+    },
+    company: {
+      type: String,
+      required: true,
+    },
+    position: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+    },
+    type: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
+    applyUrl: {
+      type: String,
+      // may change to not required
+      required: true,
+    },
+    createAt: {
+      type: String,
+      required: true,
+    },
+    comment: {
+      type: String,
+    },
   },
 });
 
